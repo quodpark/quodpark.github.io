5a40b71b2bb2be4f6e4da04289574f8fd657a805
quodpark <126001945+quodpark@users.noreply.github.com>
2025-07-04 08:47:48 +0000

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,7 +124,6 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-feed`,
-    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-lodash`,
